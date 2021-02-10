de287cbf85c4e6cfb82535c18091d20c868573e7
lps <costezki.eugen@gmail.com>
2021-02-10 23:00:32 +0100

    adjustments to the test_bash_ml_flow.sh and README.md

--- mlflow/test_bash_ml_flow.sh
+++ mlflow/test_bash_ml_flow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source .env
+source ../.env
 
 export AWS_ACCESS_KEY_ID=$MINIO_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$MINIO_SECRET_KEY
@@ -8,5 +8,8 @@ export MLFLOW_S3_ENDPOINT_URL=$MLFLOW_S3_ENDPOINT_URL
 export MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI
 
 echo "[ OK ] Successfully set the environment variables!"
+
+echo "Warning: make sure that the **mlflow** Python library is available to teh python interpreter.
+We recommend setting up an virtual environment and running *pip install mlflow* there."
 
 python tests/ml_flow_demo.py
